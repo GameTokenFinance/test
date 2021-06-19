2e4783d10631696f2aac405f3ec0ed237c8b7405
gametokenfinance <info@gametoken.finance>
2021-06-19 23:53:54 +0300

    ts

--- src/config/constants/index.ts
+++
@@ -1,7 +0,0 @@
-import farmsConfig from './farms'
-
-const communityFarms = farmsConfig.filter((farm) => farm.isCommunity).map((farm) => farm.token.symbol)
-
-export { farmsConfig, communityFarms }
-export { default as poolsConfig } from './pools'
-export { default as ifosConfig } from './ifo'
